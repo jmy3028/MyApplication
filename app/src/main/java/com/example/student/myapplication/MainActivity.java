f5287af815886eab0ac44a0d5767ed927d72db04
MinYoung.Jung <jmy3028@gmail.com>
2017-01-25 11:49:11 +0900

    커피 앱 연습 : 버튼 클릭, Log,Toast 연습2

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.student.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private Button mMinusButton;
     private Button mPlusButton;
     private TextView mQuantityTextView;
+    private TextView mResultTextView;
+    private Button mOrderButton;
 
     //수량
     private int mQuantity;
@@ -32,26 +33,67 @@ public class MainActivity extends AppCompatActivity {
         //레이아웃에서 특정 id를 인스턴스 변수와 연결
         mMinusButton = (Button) findViewById(R.id.minus_button);
         mPlusButton = (Button) findViewById(R.id.plus_button);
-        mQuantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        mOrderButton = (Button) findViewById(R.id.order_button);
+        mQuantityTextView = (TextView) findViewById(R.id.number_text_view);
+        mResultTextView = (TextView) findViewById(R.id.total_text_view);
 
         //무명클래스
         mMinusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //debug
-                Log.d(TAG,"마이너스 버튼 클릭");
-                Log.v(TAG,"일반로그");
-                Log.e(TAG,"에러로그");
-                Log.i(TAG,"정보로그");
-                Log.w(TAG,"경고로그");
 
-                //토스트 메세지
-                Toast.makeText(MainActivity.this,"메세지",Toast.LENGTH_SHORT).show();
+                mQuantity--;
+                if (mQuantity < 0) {
+                    mQuantity = 0;
+                }
 
+                displayResult();
+
+
+//                //debug
+//                Log.d(TAG,"마이너스 버튼 클릭");
+//                Log.v(TAG,"일반로그");
+//                Log.e(TAG,"에러로그");
+//                Log.i(TAG,"정보로그");
+//                Log.w(TAG,"경고로그");
+//
+//                //토스트 메세지
+//                Toast.makeText(MainActivity.this,"메세지",Toast.LENGTH_SHORT).show();
+//
+
+            }
+        });
+        //무명클래스
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mQuantity++;
+                if (mQuantity > 10) {
+                    mQuantity = 10;
+                }
+
+
+                displayResult();
+            }
+        });
+
+        mOrderButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String message = mResultTextView.getText().toString();
+                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
 
             }
         });
 
+    }
+
+    private void displayResult() {
+        mQuantityTextView.setText("" + mQuantity);
+        int price = 3000;
+        String result = "가격 : " + (price * mQuantity) + "원 입니다.\n 감사합니다!";
+        mResultTextView.setText(result);
     }
 
     private void init() {
