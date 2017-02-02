5c7b13dcbb24a24b39ba34ce775eed7836a6915a
MinYoung.Jung <jmy3028@gmail.com>
2017-02-02 10:53:53 +0900

    Option menu연습

--- app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.student.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final int price = 3000;
+    private static int mIsCreampay = 500;
     private static final String TAG = MainActivity.class.getSimpleName();
     //모델변수에다가는 변수이름 앞에 m을 안붙일거고 모델변수가 아닌 건 m을 붙일거다
     private Button mMinusButton;
@@ -16,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mQuantityTextView;
     private TextView mResultTextView;
     private Button mOrderButton;
+    private CheckBox mCreamCheckBox;
+    private boolean mIsCream;
 
     //수량
     private int mQuantity;
@@ -36,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         mOrderButton = (Button) findViewById(R.id.order_button);
         mQuantityTextView = (TextView) findViewById(R.id.number_text_view);
         mResultTextView = (TextView) findViewById(R.id.total_text_view);
+        mCreamCheckBox = (CheckBox) findViewById(R.id.cream_check);
 
         //무명클래스
         mMinusButton.setOnClickListener(new View.OnClickListener() {
@@ -78,21 +89,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mOrderButton.setOnClickListener(new View.OnClickListener(){
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 String message = mResultTextView.getText().toString();
-                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+//                Intent intent = new Intent(this,);
+//                startActivity(intent);
 
             }
         });
 
+        mCreamCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mIsCream = isChecked;
+                displayResult();
+            }
+        });
     }
 
     private void displayResult() {
         mQuantityTextView.setText("" + mQuantity);
-        int price = 3000;
-        String result = "가격 : " + (price * mQuantity) + "원 입니다.\n 감사합니다!";
+        int totalprice = price * mQuantity;
+        if (mIsCream == true) {
+            mIsCreampay = mQuantity * mIsCreampay;
+        } else {
+            mIsCreampay = 0;
+        }
+
+        String result = String.format("가격 : %d원\n수량 : %d개\n휘핑크림 : %s\n감사합니다",
+                totalprice + mIsCreampay, mQuantity, mIsCream);
+
         mResultTextView.setText(result);
     }
 
@@ -100,5 +129,28 @@ public class MainActivity extends AppCompatActivity {
         mQuantity = 0;
     }
 
+    //메뉴를 붙이는 부분
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_setting:
+                Toast.makeText(this,"설정 아직 미구현",Toast.LENGTH_SHORT);
+                return true;
+            case R.id.action_Item2:
+                return true;
+            case R.id.action_Item3:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
